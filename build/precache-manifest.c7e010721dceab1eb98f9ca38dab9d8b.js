febcb8577187ce6b81db241590b21c9410413b29
Dhruv Rohatgi <dhruvrohatgi53@gmail.com>
2021-12-26 05:56:10 +0000

    production build test

---
+++ build/precache-manifest.c7e010721dceab1eb98f9ca38dab9d8b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "127529dd68dcb92089dd",
+    "url": "/static/css/main.19a5c74f.chunk.css"
+  },
+  {
+    "revision": "127529dd68dcb92089dd",
+    "url": "/static/js/main.127529dd.chunk.js"
+  },
+  {
+    "revision": "396c46cc417f0676beda",
+    "url": "/static/css/1.1eae6a62.chunk.css"
+  },
+  {
+    "revision": "396c46cc417f0676beda",
+    "url": "/static/js/1.396c46cc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "248156bebae440f2441d1d66b5a4b4b4",
+    "url": "/static/media/brain.248156be.m4v"
+  },
+  {
+    "revision": "afa683b739e9a325faf88f55220306f5",
+    "url": "/static/media/analytics1.afa683b7.jpg"
+  },
+  {
+    "revision": "da47c7b69ca169d74b7262c543bd364e",
+    "url": "/static/media/sol3.da47c7b6.jpg"
+  },
+  {
+    "revision": "6c8cff7ce1057835a217bfc621ddee3f",
+    "url": "/static/media/sol4.6c8cff7c.jpg"
+  },
+  {
+    "revision": "57cb1c7098e9e530858afb7fd6cf6610",
+    "url": "/static/media/about.57cb1c70.mp4"
+  },
+  {
+    "revision": "93b9e4d7ad0d46177a0474a5eba5b233",
+    "url": "/static/media/about.93b9e4d7.jpg"
+  },
+  {
+    "revision": "7cd652c02385aafccecb5798802bf716",
+    "url": "/static/media/mission.7cd652c0.jpg"
+  },
+  {
+    "revision": "154914f44179d7fad44c68b266cd7848",
+    "url": "/index.html"
+  }
+];
